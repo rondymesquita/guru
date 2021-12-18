9ab1d042d7ccc6f3a0f4bddd6f360b4a136a5505
rondymesquita <rondinellimesquitas@gmail.com>
2021-12-18 15:10:19 -0300

    feat: add base path for vite build

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+
+let base;
+
+if (process.env.NODE_ENV === 'production') {
+  base = '/guru/';
+} else {
+  base = '/';
+}
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
-})
+  base,
+  plugins: [vue()],
+});
